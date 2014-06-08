24c0562e317b251fbc9b497768835e969683e54f
Glynn Forrest <me@glynnforrest.com>
2014-06-08 15:29:36 -0500

    Adding RequestListener to automatically assign the Request to a driver.

---
+++ src/Blockade/EventListener/RequestListener.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Blockade\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+use Blockade\Driver\DriverInterface;
+
+/**
+ * RequestListener sets the current Request to a Driver.
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class RequestListener implements EventSubscriberInterface
+{
+
+    protected $drivers = array();
+
+    /**
+     * Add a driver. It will be given the request on a kernel request
+     * event.
+     *
+     * @param  DriverInterface $driver The driver
+     * @return RequestListener This listener instance
+     */
+    public function addDriver(DriverInterface $driver)
+    {
+        $this->drivers[] = $driver;
+
+        return $this;
+    }
+
+    /**
+     * Get all drivers.
+     *
+     * @return array An array of drivers
+     */
+    public function getDrivers()
+    {
+        return $this->drivers;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        foreach ($this->drivers as $driver) {
+            $driver->setRequest($request);
+        }
+
+        return true;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::REQUEST => array('onKernelRequest')
+        );
+    }
+
+}
---
+++ tests/Blockade/Tests/EventListener/RequestListenerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Blockade\Tests\EventListener;
+
+use Blockade\EventListener\RequestListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\Request;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+/**
+ * RequestListenerTest
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class RequestListenerTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $listener;
+
+    public function setUp()
+    {
+        $this->listener = new RequestListener();
+    }
+
+    public function testGetSubscribedEvents()
+    {
+        $expected = array(KernelEvents::REQUEST => array('onKernelRequest'));
+        $this->assertSame($expected, RequestListener::getSubscribedEvents());
+    }
+
+    public function testAddAndGetDrivers()
+    {
+        $this->assertSame(array(), $this->listener->getDrivers());
+        $driver = $this->getMock('Blockade\Driver\DriverInterface');
+        $this->assertSame($this->listener, $this->listener->addDriver($driver));
+        $this->assertSame(array($driver), $this->listener->getDrivers());
+        $this->assertSame($this->listener, $this->listener->addDriver($driver));
+        $this->assertSame(array($driver, $driver), $this->listener->getDrivers());
+    }
+
+    public function testOnKernelRequest()
+    {
+        $request = new Request();
+        $event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\GetResponseEvent')
+                      ->disableOriginalConstructor()
+                      ->getMock();
+
+        $event->expects($this->once())
+              ->method('getRequest')
+              ->will($this->returnValue($request));
+
+        $driver = $this->getMock('Blockade\Driver\DriverInterface');
+        $this->listener->addDriver($driver)
+                       ->addDriver($driver);
+
+        $driver->expects($this->exactly(2))
+                      ->method('setRequest')
+                      ->with($request);
+
+        $this->listener->onKernelRequest($event);
+
+    }
+
+}
